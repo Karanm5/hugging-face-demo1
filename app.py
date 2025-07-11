3fd710429dfd4c70c88c50cbafb195f51db1bcc9
Karan <meena.karan9k@gmail.com>
2025-07-11 09:51:40 +0000

    Trial code

---
+++ app.py
@@ -0,0 +1,17 @@
+from transformers import pipeline
+import gradio as gr
+
+
+model = pipeline("summerization")
+
+
+def predict(prompt):
+    summary = model(prompt)[0]["summary_text"]
+    return summary
+
+
+with gr.Blocks() as demo:
+    textbox = gr.Textbox(placeholder="Enter text block to summerize", lines=4)
+    gr.Interface(fn=predict, inputs=textbox, output="text")
+
+demo.launch()
